083e8490175aa9c4f83f6a2c986480fb4e50177a
Tianon Gravi <admwiggin@gmail.com>
2016-08-09 12:40:30 -0700

    Convert "generate-stackbrew-library.sh" to output the new 2822-based format

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-url='git://github.com/piwik/docker-piwik.git'
+url='https://github.com/piwik/docker-piwik.git'
 
 commit="$(git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
 fullVersion="$(grep -m1 'ENV PIWIK_VERSION ' ./Dockerfile | cut -d' ' -f3)"
 
-echo '# maintainer: Pierre Ozoux <pierre@piwik.org> (@pierreozoux)'
-echo
-echo "$fullVersion: ${url}@${commit}"
-echo "${fullVersion%.*}: ${url}@${commit}"
-echo "${fullVersion%.*.*}: ${url}@${commit}"
-echo "latest: ${url}@${commit}"
+cat <<EOF
+Maintainers: Pierre Ozoux <pierre@piwik.org> (@pierreozoux)
+GitRepo: $url
+
+Tags: $fullVersion, ${fullVersion%.*}, ${fullVersion%.*.*}, latest
+GitCommit: $commit
+EOF
