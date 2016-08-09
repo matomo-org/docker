288b37ff5d67ee64494be112ffd0df4808d3d5df
Tianon Gravi <admwiggin@gmail.com>
2016-08-09 09:59:33 -0700

    Update generated "maintainer:" comment line

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -6,7 +6,7 @@ url='git://github.com/piwik/docker-piwik.git'
 commit="$(git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
 fullVersion="$(grep -m1 'ENV PIWIK_VERSION ' ./Dockerfile | cut -d' ' -f3)"
 
-echo '# maintainer pierre@piwik.org'
+echo '# maintainer: Pierre Ozoux <pierre@piwik.org> (@pierreozoux)'
 echo
 echo "$fullVersion: ${url}@${commit}"
 echo "${fullVersion%.*}: ${url}@${commit}"
