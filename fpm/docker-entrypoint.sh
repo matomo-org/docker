efc264d628c2610afdc6920b62293e6066f6475d
Tobias Florek <tob@butter.sh>
2018-03-08 14:04:46 +0100

    allow running as non-root
    fixes #55

--- fpm/docker-entrypoint.sh
+++ fpm/docker-entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 if [ ! -e matomo.php ]; then
 	tar cf - --one-file-system -C /usr/src/matomo . | tar xf -
-	chown -R www-data .
+    if [ "$(id -u)" -eq 0 ]; then
+        chown -R www-data .
+    fi
 fi
 
 exec "$@"
