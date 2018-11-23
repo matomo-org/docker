bf06118a1ad77fe6b1aeaf3cfe7c906680abf725
J0WI <J0WI@users.noreply.github.com>
2018-11-23 15:06:57 +0100

    Update pecl extensions in update.sh

--- update.sh
+++ update.sh
@@ -21,6 +21,32 @@ latest="$(
 		| tail -1
 )"
 
+apcu_version="$(
+	git ls-remote --tags https://github.com/krakjoe/apcu.git \
+		| cut -d/ -f3 \
+		| grep -vE -- '-rc|-b' \
+		| sed -E 's/^v//' \
+		| sort -V \
+		| tail -1
+)"
+
+# Todo: check for 4.x compatibility
+redis_version="$(
+	git ls-remote --tags https://github.com/phpredis/phpredis.git \
+		| cut -d/ -f3 \
+		| grep -viE '[a-z]' \
+		| tr -d '^{}' \
+		| grep -E '^3\.' \
+		| sort -V \
+		| tail -1
+)"
+
+declare -A pecl_versions=(
+	[APCu]="$apcu_version"
+	[geoip]="1.1.1" # Todo: fetch latest tag from SVN repo
+	[redis]="$redis_version"
+)
+
 set -x
 
 for variant in apache fpm fpm-alpine; do
@@ -31,6 +57,9 @@ for variant in apache fpm fpm-alpine; do
 	sed -ri -e '
 		s/%%VARIANT%%/'"$variant"'/;
 		s/%%VERSION%%/'"$latest"'/;
+		s/%%APCU_VERSION%%/'"${pecl_versions[APCu]}"'/g;
+		s/%%GEOIP_VERSION%%/'"${pecl_versions[geoip]}"'/g;
+		s/%%REDIS_VERSION%%/'"${pecl_versions[redis]}"'/g;
 		s/%%CMD%%/'"${cmd[$variant]}"'/;
 	' "$variant/Dockerfile"
 done
