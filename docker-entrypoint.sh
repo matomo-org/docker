beed08465e7930eeb54fe15351b883d38b95c769
Paul Tiedtke <PaulTiedtke@web.de>
2020-02-23 03:16:05 +0100

    Run container by unprivileged user and speed up container start

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -e
 
-if [ ! -e matomo.php ]; then
-	tar cf - --one-file-system -C /usr/src/matomo . | tar xf -
-	chown -R www-data .
+if [ ! -e "$APACHE_DOCUMENT_ROOT/config/global.ini.php" ]; then
+  ln -s /usr/src/config/* "$APACHE_DOCUMENT_ROOT/config"
+fi
+
+if [ ! \( -n "$(ls -A "$APACHE_DOCUMENT_ROOT/plugins")" \) ]; then
+  ln -s /usr/src/plugins/* "$APACHE_DOCUMENT_ROOT/plugins"
 fi
 
 exec "$@"
