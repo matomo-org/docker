8c150f1d7c1c8cb19f1eefbfd154a3284e90f62f
Jakob Helmecke <jakob.helmecke@yeebase.com>
2020-09-15 12:59:24 +0200

    Prepare for kubernetes

--- apache/common.config.ini.php
+++ apache/common.config.ini.php
@@ -1,3 +1,8 @@
+[General]
+enable_trusted_host_check = 0
+multi_server_environment = 1
+enable_plugins_admin = 0
+
 [Plugins]
 Plugins[] = EnvironmentVariables
 
--- update.sh
+++ update.sh
@@ -25,7 +25,7 @@ set -x
 
 for variant in apache fpm fpm-alpine; do
 	template="Dockerfile-${base[$variant]}.template"
-	cp $template "$variant/Dockerfile"
+	cp "$template" "$variant/Dockerfile"
 	cp docker-entrypoint.sh "$variant/docker-entrypoint.sh"
 	cp common.config.ini.php "$variant/common.config.ini.php"
 	cp php.ini "$variant/php.ini"
