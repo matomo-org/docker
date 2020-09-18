6f4ef9813b8fda70594c9c04f26077662126455a
Max Schmidt <m.schmidt@yeebase.com>
2020-09-18 12:31:52 +0200

    fix indent

--- fpm-alpine/docker-entrypoint.sh
+++ fpm-alpine/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ ! -e matomo.php ]; then
 	tar cf - --one-file-system -C /usr/src/matomo . | tar xf -
-    ln -s /usr/src/matomo/DBIP-City.mmdb /var/www/html/misc/DBIP-City.mmdb
+	ln -s /usr/src/matomo/DBIP-City.mmdb /var/www/html/misc/DBIP-City.mmdb
 	chown -R www-data:www-data .
 fi
 
