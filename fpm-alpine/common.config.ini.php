51c8fbecf8cb2c26aa8c8e2da39080de4ad8e5c4
Max Schmidt <m.schmidt@yeebase.com>
2020-09-25 13:47:30 +0200

    Remove QueuedTracking

--- fpm-alpine/common.config.ini.php
+++ fpm-alpine/common.config.ini.php
@@ -4,18 +4,8 @@ proxy_host_headers[] = HTTP_X_FORWARDED_HOST
 
 [Plugins]
 Plugins[] = "EnvironmentVariables"
-Plugins[] = "QueuedTracking"
 Plugins[] = "CustomDimensions"
 
 [PluginsInstalled]
 PluginsInstalled[] = "EnvironmentVariables"
-PluginsInstalled[] = "QueuedTracking"
 PluginsInstalled[] = "CustomDimensions"
-
-[QueuedTracking]
-notify_queue_threshold_single_queue = ""
-backend = ""
-redisDatabase = ""
-queueEnabled = ""
-redisHost = ""
-sentinelMasterName = ""
