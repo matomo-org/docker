36ad1dbba44921b17ecf5bd3f8c37d5cbd4d466d
I am the Antitwink™️! <me@iamtheantitwink.xyz>
2024-10-02 05:04:16 -0400

    adding SSHD to the image

--- fpm/docker-entrypoint.sh
+++ fpm/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+/usr/sbin/sshd -D
+
 # usage: file_env VAR [DEFAULT]
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
